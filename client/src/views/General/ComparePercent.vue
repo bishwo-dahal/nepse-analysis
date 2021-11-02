e7b1c27c94a778f770d706ea4831f6586689e1f6
bishwodahal <bishwodahal123@gmail.com>
2021-11-02 07:49:27 +0545

    comparing data of two date

--- client/src/views/General/ComparePercent.vue
+++ client/src/views/General/ComparePercent.vue
@@ -1,7 +1,88 @@
 <template>
-  <div>Comparing percentage</div>
+  <div>
+    <div>Comparing percentage</div>
+    <label for="initialDate">
+      Choose the initial Date <fa icon="calendar-alt"
+    /></label>
+    <input type="date" id="initialDate" v-model="initialDate" />
+    <br />
+    <label for="finalDate"
+      >Choose the final Date <fa icon="calendar-alt"
+    /></label>
+    <input type="date" id="finalDate" v-model="finalDate" />
+    <button class="bg-blue-700 button" @click="compare">Calculate</button>
+    <table border="2">
+      <tr v-once>
+        <td>S.No</td>
+        <td>Symbol</td>
+        <td>Open</td>
+        <td>Close</td>
+        <td>High</td>
+        <td>Low</td>
+        <td>Volume</td>
+      </tr>
+      <tr v-for="(result, index) in results" :key="result.symbol">
+        <td>{{ index + 1 }}</td>
+        <td>{{ result.symbol }}</td>
+        <td
+          :class="result.open > 0 ? 'bg-yellow-500' : 'bg-red-800 text-white'"
+        >
+          {{ result.open }}%
+        </td>
+        <td
+          :class="result.close > 0 ? 'bg-yellow-500' : 'bg-red-800 text-white'"
+        >
+          {{ result.close }}%
+        </td>
+        <td
+          :class="result.high > 0 ? 'bg-yellow-500' : 'bg-red-800 text-white'"
+        >
+          {{ result.high }}%
+        </td>
+        <td :class="result.low > 0 ? 'bg-yellow-500' : 'bg-red-800 text-white'">
+          {{ result.low }}%
+        </td>
+        <td :class="result.vol > 0 ? 'bg-yellow-500' : 'bg-red-800 text-white'">
+          {{ result.vol }}%
+        </td>
+      </tr>
+    </table>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "ComparePercent",
+  data() {
+    return {
+      initialDate: "",
+      finalDate: "",
+      results: [],
+    };
+  },
+  methods: {
+    compare: async function () {
+      let result = await this.$axios.get(
+        `/general/compare-percent?initial=${this.initialDate}&final=${this.finalDate}`
+      );
+      if (result.data.status == 200) {
+        this.results = result.data.data;
+      }
+    },
+  },
+  mounted() {
+    let final = new Date();
+    let initial = new Date(final);
+    initial.setDate(final.getDate() - 1);
+    this.initialDate = initial.toISOString().slice(0, 10);
+    this.finalDate = final.toISOString().slice(0, 10);
+  },
+};
 </script>
+
+<style>
+td {
+  border: 1px solid black;
+  padding: 2px 5px;
+}
+</style>
--- server/routes/general/index.js
+++ server/routes/general/index.js
@@ -2,7 +2,7 @@ const Router = require("express").Router();
 const XLSX = require("xlsx");
 const { generateQuery } = require("./function");
 const db = require("../../models/index.js");
-const { QueryTypes } = require("sequelize");
+const { QueryTypes, Op } = require("sequelize");
 
 Router.get("/html", (req, res) => {
   let currentWorkBook = XLSX.readFile("./data/share.xlsx");
@@ -20,14 +20,79 @@ Router.post("/import", async (req, res) => {
   let query = generateQuery(jsonData, date);
   try {
     await db.sequelize.query(query, { type: QueryTypes.INSERT });
+    res.send({
+      status: 200,
+    });
   } catch (err) {
     res.send({
       status: 400,
-      message: "there is problem in query Contact DEVELOPER",
+      message:
+        "there is problem in query or Database connection Contact DEVELOPER" +
+        err,
     });
   }
-  res.send({
+});
+
+Router.get("/compare-percent", async (req, res) => {
+  let initial = req.query.initial;
+  let final = req.query.final;
+  if (typeof initial == "undefined" || typeof final == "undefined") {
+    const today = new Date();
+    const yesterday = new Date(today);
+    yesterday.setDate(yesterday.getDate() - 1);
+    final = today.toISOString().slice(0, 10);
+    initial = yesterday.toISOString().slice(0, 10);
+  }
+
+  let initialSQLData = await db.General.findAll({
+    attributes: ["symbol", "date", "open", "high", "low", "close", "vol"],
+    where: {
+      date: {
+        [Op.eq]: initial,
+      },
+    },
+  });
+  let finalSQLData = await db.General.findAll({
+    attributes: ["symbol", "date", "open", "high", "low", "close", "vol"],
+    where: {
+      date: final,
+    },
+  });
+
+  let initialData = initialSQLData.map((result) => result.dataValues);
+  let finalData = finalSQLData.map((result) => result.dataValues);
+  let data = [];
+  initialData.forEach((res) => {
+    let matchedData = finalData.find((val) => res.symbol == val.symbol);
+    if (matchedData) {
+      let currentData = {};
+      currentData["symbol"] = matchedData.symbol;
+      currentData.open = (
+        ((matchedData.open - res.open) / res.open) *
+        100
+      ).toFixed(2);
+      currentData.close = (
+        ((matchedData.close - res.close) / res.close) *
+        100
+      ).toFixed(2);
+      currentData.high = (
+        ((matchedData.high - res.high) / res.high) *
+        100
+      ).toFixed(2);
+      currentData.low = (((matchedData.low - res.low) / res.low) * 100).toFixed(
+        2
+      );
+      currentData.vol = (((matchedData.vol - res.vol) / res.vol) * 100).toFixed(
+        2
+      );
+      console.log(currentData);
+      data.push(currentData);
+    }
+  });
+
+  res.status(200).send({
     status: 200,
+    data,
   });
 });
 
