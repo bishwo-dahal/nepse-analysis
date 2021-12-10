642a4917a8d16ea7276480ba90be8222b89c9b64
Bishwodahal <61019968+Bishwodahal@users.noreply.github.com>
2021-12-10 13:05:19 +0545

    charts created

--- client/src/views/Dashboard/Dashboard.vue
+++ client/src/views/Dashboard/Dashboard.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <canvas id="stock-chart" class="w-full"></canvas>
+    <div style="position: relative; height: 40vh; width: 98vw">
+      <canvas id="stock-chart"></canvas>
+    </div>
   </div>
 </template>
 
@@ -9,10 +11,71 @@ import Chart from "chart.js/auto";
 export default {
   name: "Dashboard",
   components: {},
-  mounted() {
+  data() {
+    return {
+      tradedInfo: [],
+    };
+  },
+  methods: {
+    fetchTraded: async function () {
+      try {
+        let tradedInfo = await this.$axios.get("/dashboard/");
+        let traded = await tradedInfo.data;
+        let tradedStatus = await traded.status;
+        if (tradedStatus == 200) {
+          let tradedResult = await traded.result;
+          return tradedResult;
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+  mounted: async function () {
+    let tradedInfo = await this.fetchTraded();
+    let labels = [];
+    let turnover = [];
+    let companies = [];
+    let volume = [];
+    tradedInfo.forEach((currentTrade) => {
+      labels.push(currentTrade.date);
+      turnover.push(currentTrade.turnover);
+      companies.push(currentTrade.companies);
+      volume.push(currentTrade.volume);
+    });
+
     const ctx = document.getElementById("stock-chart").getContext("2d");
+    let data = {
+      labels: labels,
+      datasets: [
+        {
+          label: "companies",
+          data: companies,
+          tension: 0.3,
+          borderColor: "red",
+          fill: true,
+        },
+        {
+          label: "TurnOver",
+          data: turnover,
+          tension: 0.3,
+          borderColor: "blue",
+          fill: true,
+        },
+        {
+          label: "Volume",
+          data: volume,
+          tension: 0.3,
+          borderColor: "green",
+          fill: true,
+        },
+      ],
+    };
+    console.log(tradedInfo);
+
     const config = {
       type: "line",
+      data: data,
     };
     new Chart(ctx, config);
   },
--- server/routes/dashboard/dashboard.js
+++ server/routes/dashboard/dashboard.js
@@ -1,9 +1,22 @@
 const Router = require("express").Router();
 const db = require("../../models/index.js");
-const { QueryTypes, Op, json } = require("sequelize");
 const errors = require("../../errors");
 const { getResponse } = require("../../response");
 
-Router.get("/", async (req, res) => {});
+Router.get("/", async (req, res) => {
+  let currentErrors = [];
+  let result = {};
+  try {
+    let tradedInfo = await db.Traded.findAll({
+      raw: true,
+      attributes: { exclude: ["s_no"] },
+    });
+
+    result = tradedInfo;
+  } catch (error) {
+    currentErrors.push(error);
+  }
+  res.send(getResponse(currentErrors, result));
+});
 
 module.exports = Router;
