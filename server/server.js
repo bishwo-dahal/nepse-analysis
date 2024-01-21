44be203d8f6e6a119ff9882091314fdc13eeb83f
Bishwodahal <61019968+Bishwodahal@users.noreply.github.com>
2024-01-21 17:42:55 -0600

    port updated

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 3070;
+const PORT = process.env.PORT || 3070;
 const HOST = "localhost";
 const cors = require("cors");
 app.use(express.json());
